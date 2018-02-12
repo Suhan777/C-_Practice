d0373da5d0494031a26b669c4af5ff2289208df1
Suhan Muppavaram <suhan.m@hotmail.com>
2018-02-12 13:13:16 +1300

    hello world

---
+++ Practice1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hello world");
+
+
+
+
+        }
+    }
+}
