e524462bf850a0f4a753d7a96a82f77fe9804a45
Suhan Muppavaram <suhan.m@hotmail.com>
2018-02-12 13:20:51 +1300

    changed console colour

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -7,9 +7,14 @@ namespace Practice1
     {
         static void Main(string[] args)
         {
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Suhan";
 
-            Console.WriteLine("hello world");
-
+            //Change text colour
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+            Console.ResetColor();
 
 
 
